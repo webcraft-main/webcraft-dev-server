ae505a8125352c5d63ec7cfb279eb7013dc800f7
Advik Abhilash <advik.abhilash2014@outlook.com>
2026-04-23 19:53:58 -0400

    Refactor server.js for improved structure and clarity

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 // ==========================================
 // Server
-//
-// This file contains all of the code necessary for managing a
-// WebCraft server on the Node.js platform.
 // ==========================================
 
 // Parameters
@@ -15,94 +12,88 @@ var ADMIN_IP = "";
 
 // Load modules
 var modules = {};
-modules.helpers = require( "./js/helpers.js" );
-modules.blocks = require( "./js/blocks.js" );
-modules.world = require( "./js/world.js" );
-modules.network = require( "./js/network.js" );
-modules.io = require( "socket.io" );
-modules.fs = require( "fs" );
-var log = require( "util" ).log;
+modules.helpers = require("./js/helpers.js");
+modules.blocks = require("./js/blocks.js");
+modules.world = require("./js/world.js");
+modules.network = require("./js/network.js");
+modules.io = require("socket.io");
+modules.fs = require("fs");
+var log = require("util").log;
 
-// Set-up evil globals
+// HTTP + Express (REQUIRED FOR RENDER)
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+
+// Evil globals
 global.Vector = modules.helpers.Vector;
 global.BLOCK = modules.blocks.BLOCK;
 
-// Create new empty world or load one from file
-var world = new modules.world.World( WORLD_SX, WORLD_SY, WORLD_SZ );
-log( "Creating world..." );
-if ( world.loadFromFile( "world" ) ) {
-	log( "Loaded the world from file." );
-} else {
-	log( "Creating a new empty world." );
-	world.createFlatWorld( WORLD_GROUNDHEIGHT );
-	world.saveToFile( "world" );
-}
+// Create world
+var world = new modules.world.World(WORLD_SX, WORLD_SY, WORLD_SZ);
+log("Creating new world...");
+world.createFlatWorld(WORLD_GROUNDHEIGHT);
 
-// Start server
-var server = new modules.network.Server( modules.io, 16 );
-server.setWorld( world );
-server.setLogger( log );
-server.setOneUserPerIp( true );
-log( "Waiting for clients..." );
+// Attach socket.io to HTTP server
+var io = modules.io(http);
+var server = new modules.network.Server(io, 16);
+server.setWorld(world);
+server.setLogger(log);
+server.setOneUserPerIp(true);
+log("Waiting for clients...");
 
 // Chat commands
-server.on( "chat", function( client, nickname, msg )
-{
-	if ( msg == "/spawn" ) {
-		server.setPos( client, world.spawnPoint.x, world.spawnPoint.y, world.spawnPoint.z );
-		return true;
-	} else if ( msg.substr( 0, 3 ) == "/tp" ) {
-		var target = msg.substr( 4 );
-		target = server.findPlayerByName( target );
-		
-		if ( target != null ) {
-				server.setPos( client, target.x, target.y, target.z );
-				server.sendMessage( nickname + " was teleported to " + target.nick + "." );
-				return true;
-		} else {
-			server.sendMessage( "Couldn't find that player!", client );
-			return false;
-		}
-	} else if ( msg.substr( 0, 5 ) == "/kick" && client.handshake.address.address == ADMIN_IP ) {
-		var target = msg.substr( 6 );
-		target = server.findPlayerByName( target );
-		
-		if ( target != null ) {
-				server.kick( target.socket, "Kicked by Overv" );
-				return true;
-		} else {
-			server.sendMessage( "Couldn't find that player!", client );
-			return false;
-		}
-	} else if ( msg == "/list" ) {
-		var playerlist = "";
-		for ( var p in world.players )
-			playerlist += p + ", ";
-		playerlist = playerlist.substring( 0, playerlist.length - 2 );
-		server.sendMessage( "Players: " + playerlist, client );
-		return true;
-	} else if ( msg.substr( 0, 1 ) == "/" ) {
-		server.sendMessage( "Unknown command!", client );
-		return false;
-	}
-} );
+server.on("chat", function (client, nickname, msg) {
+    if (msg == "/spawn") {
+        server.setPos(client, world.spawnPoint.x, world.spawnPoint.y, world.spawnPoint.z);
+        return true;
+    } else if (msg.substr(0, 3) == "/tp") {
+        var target = msg.substr(4);
+        target = server.findPlayerByName(target);
 
-// Send a welcome message to new clients
-server.on( "join", function( client, nickname )
-{
-	server.sendMessage( "Welcome! Enjoy your stay, " + nickname + "!", client );
-	server.broadcastMessage( nickname + " joined the game.", client );
-} );
+        if (target != null) {
+            server.setPos(client, target.x, target.y, target.z);
+            server.sendMessage(nickname + " was teleported to " + target.nick + ".");
+            return true;
+        } else {
+            server.sendMessage("Couldn't find that player!", client);
+            return false;
+        }
+    } else if (msg.substr(0, 5) == "/kick" && client.handshake.address.address == ADMIN_IP) {
+        var target = msg.substr(6);
+        target = server.findPlayerByName(target);
 
-// And let players know of a disconnecting user
-server.on( "leave", function( nickname )
-{
-	server.sendMessage( nickname + " left the game." );
-} );
+        if (target != null) {
+            server.kick(target.socket, "Kicked by Overv");
+            return true;
+        } else {
+            server.sendMessage("Couldn't find that player!", client);
+            return false;
+        }
+    } else if (msg == "/list") {
+        var playerlist = "";
+        for (var p in world.players) playerlist += p + ", ";
+        playerlist = playerlist.substring(0, playerlist.length - 2);
+        server.sendMessage("Players: " + playerlist, client);
+        return true;
+    } else if (msg.substr(0, 1) == "/") {
+        server.sendMessage("Unknown command!", client);
+        return false;
+    }
+});
 
-// Periodical saves
-setInterval( function()
-{
-	world.saveToFile( "world" );
-	log( "Saved world to file." );
-}, SECONDS_BETWEEN_SAVES * 1000 );
+// Join/leave messages
+server.on("join", function (client, nickname) {
+    server.sendMessage("Welcome! Enjoy your stay, " + nickname + "!", client);
+    server.broadcastMessage(nickname + " joined the game.", client);
+});
+
+server.on("leave", function (nickname) {
+    server.sendMessage(nickname + " left the game.");
+});
+
+// Start server (REQUIRED FOR RENDER)
+const PORT = process.env.PORT || 8080;
+http.listen(PORT, () => {
+    log("Server running on port " + PORT);
+});
